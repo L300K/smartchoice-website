119e6dde1a411bc9aa955f633c5169b642c2f14b
Sawarach Silakorn <93990763+L300K@users.noreply.github.com>
2025-04-08 04:58:59 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/smartchoice-website',
-  assetPrefix: '/smartchoice-website/',
 };
 
 export default nextConfig;
